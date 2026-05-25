3deb93e5852685b059d8e96cbf38d3e7d15feeea
L3monScent <lupusfetch@outlook.com>
2026-05-25 09:37:38 +0100

    Exit Codes / install requirements
    - updated pip install requests to install -r requirements.txt
    
    - added exit codes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3
 COPY zoom_monitor.py .
-RUN pip install requests
+RUN pip install -r  requirements.txt
 CMD python zoom_monitor.py
--- zoom_monitor.py
+++ zoom_monitor.py
@@ -1,10 +1,13 @@
 import requests
+import sys
 
 def get_zoom_status():
     url = "https://status.zoom.us/api/v2/summary.json"
     response = requests.get(url)
     data = response.json()
     print("=== ZOOM Service STATUS === \n")
+
+    all_operational = True
 
     for component in data["components"]:
         name = component["name"]
@@ -13,6 +16,11 @@ def get_zoom_status():
             icon = "✅"
         else:
             icon = "❌"
+            all_operational = False
         print(f"{icon} {name}: {status}")
-
+    if all_operational:
+        SystemExit(0)
+    else:
+        SystemExit(1)
+        
 get_zoom_status()
